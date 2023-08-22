4b8f46dfb858cc48a09210dad3655baae05ebaf1
hlmj3 <hlmj3017@naver.com>
2023-08-22 14:33:35 +0900

    Update 기능 구현

--- posts/views.py
+++ posts/views.py
@@ -44,3 +44,30 @@ def delete(request, id):
     post.delete()
 
     return redirect('/index/')
+
+
+def edit(request, id):
+    post = Post.objects.get(id=id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'edit.html', context)
+
+
+def update(request, id):
+    # 기존데이터
+    post = Post.objects.get(id=id)
+
+    # 방금 수정한 데이터
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    # post = Post() => 새로운 게시물 만들 때
+    # 기존데이터
+    post.title = title
+    post.content = content
+    post.save()
+
+    return redirect(f'/posts/{post.id}/')
