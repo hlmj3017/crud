d24e39979f025101ff94ecbd38cd40176268065c
hlmj3 <hlmj3017@naver.com>
2023-08-22 11:21:22 +0900

    Read(All)기능 구현

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+# Post 모델을 import
+from .models import Post
 
 # Create your views here.
-
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'index.html', context)
 
 
